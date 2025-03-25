1540068972a6ce6b74ad45deaf52d1a4d8fcec17
Rimantas <belauzas@gmail.com>
2025-03-25 08:50:19 +0200

    Up

---
+++ task/text-size.js
@@ -0,0 +1,7 @@
+/*
+Reikalinga funkcija, kuri gauna teksta, kuri grazina rezultata, priklausomai nuo teksto ilgio.
+- jei tuscias: "Tuscias tekstas."
+- jei maziau 10 simbliu: "Trumpas tekstas is N simboliu."
+- jei maziau 20 simbliu: "Tekstas is N simboliu."
+- jei ilgesnis: "Ilgas tekstas is N simboliu."
+*/
