b6bce1b853e7fa8560f0b298c86f4f33ec63acbd
Rimantas <belauzas@gmail.com>
2025-04-09 09:31:48 +0300

    Sort

--- js/19-sort.js
+++ js/19-sort.js
@@ -20,10 +20,10 @@ numbers.sort((a, b) => b - a);
 console.log(numbers);
 
 const people = [
-    { name: 'Petras', age: 77 },
-    { name: 'Ona', age: 66 },
-    { name: 'Jonas', age: 99 },
-    { name: 'Maryte', age: 88 },
+    { name: 'Petras', age: 77, isMarried: false },
+    { name: 'Ona', age: 66, isMarried: false },
+    { name: 'Jonas', age: 99, isMarried: true },
+    { name: 'Maryte', age: 88, isMarried: true },
 ];
 
 people.sort((a, b) => a.age - b.age);
@@ -32,6 +32,8 @@ console.log(people);
 people.sort((a, b) => a.name.length - b.name.length);
 console.log(people);
 
-
 people.sort((a, b) => a.name > b.name ? 1 : a.name === b.name ? 0 : -1);
+console.log(people);
+
+people.sort((a, b) => a.isMarried > b.isMarried ? 1 : a.isMarried === b.isMarried ? 0 : -1);
 console.log(people);
